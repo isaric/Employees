56543f017f7a19e1a89333fd4f441d601fe8bddb
Ivan Šarić <ivansari@gmail.com>
2016-10-31 10:31:25 +0100

    Finished adding Security layer and switched to embedded H2 database for portability.

--- src/main/java/com/isaric/SecurityConfig.java
+++ src/main/java/com/isaric/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
 		http.authorizeRequests()
-		.antMatchers("/login", "/register").permitAll()
+		.antMatchers("/login", "/register").anonymous()
 		.anyRequest().fullyAuthenticated()
 		.and()
 		.formLogin()
